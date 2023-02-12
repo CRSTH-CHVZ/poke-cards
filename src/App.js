e9f60cdaaf1e1586d6d208cf69cb595a3cbb8e3e
Cristhian-Chavez <crsth.1990@fpuna.edu.py>
2023-02-12 17:26:49 -0300

    Pkmn render data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import Layout from "./components/layout";
 import Card from "./components/card";
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 function App() {
+    const [data, setData] = useState(null);
+    async function getPkmData() {
+       return(
+           axios
+               .get(`https://pokeapi.co/api/v2/pokemon/?limit=20&offset=20%22`)
+               .then((response) => {
+                   setData(response.data.results);
+               })
+               .catch((error) => {
+                   console.error(error);
+               })
+       )
+    }
+    useEffect(() => {
+        getPkmData();
+    }, []);
+
   return (
     <>
         <Layout />
-        <Card />
+        {
+            data ? data.map( ( el, id ) => {
+                return(
+                    <Card
+                    el={ el }
+                    id={ id }
+                    />
+                )
+            })
+                : 'Sin elementos para mostrar'
+        }
     </>
   );
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
-const card = () => {
+const card = ({el, id} ) => {
+    console.log('el', el)
+    console.log('id', id)
   return (
-      <div className="card" style={{ width: '18rem'}}>
-          <img src="..." className="card-img-top" alt="..." />
+      <div className="card" style={{ width: '18rem'}} key={ id }>
+          <img src="" className="card-img-top" alt="..." />
               <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of
-                      the card's content.</p>
+                  <h5 className="card-title">{ el.name }</h5>
+                  <p className="card-text"><strong>National ID:</strong> .</p>
+                  <p className="card-text"><strong>Attack:</strong> .</p>
+                  <p className="card-text"><strong>Defense:</strong> .</p>
+                  <p className="card-text"><strong>Speed:</strong> .</p>
               </div>
       </div>
   )
